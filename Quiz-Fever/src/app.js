3b3b9256f055a94e3f14e752abf06fda7daf1c70
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-27 01:21:12 +0200

    Update app.js

--- Quiz-Fever/src/app.js
+++ Quiz-Fever/src/app.js
@@ -1,9 +1,35 @@
+import { logout } from './data/users.js';
 import { page } from './lib/lib.js';
+
 import { addGoTo } from './middlewares/redirectMiddleware.js';
 import { addRender } from './middlewares/renderMiddleware.js';
+import { session } from './middlewares/session.js';
+import { showBrowsePage } from './views/browse.js';
+import { showCreatePage } from './views/create.js';
+import { showLoginPage } from './views/login.js';
+import { loading } from './views/partials/loading.js';
+import { showProfile } from './views/profile.js';
+import { showRegisterPage } from './views/register.js';
+
+import { showHome } from './views/welcome.js';
 
 //middlewares
-page(addRender);
-page(addGoTo);
+page(session());
+page(addRender(document.getElementById('container')));
+page(addGoTo());
+
+page('/index.html', '/');
+page('/', loading(), showHome);
+page('/browse', loading(), showBrowsePage);
+page('/profile', loading(), showProfile);
+page('/create', showCreatePage);
+page('/login', showLoginPage);
+page('/register', showRegisterPage);
+page('/logout', loading(), logoutAction);
 
 page.start();
+
+async function logoutAction(ctx) {
+    await logout();
+    ctx.goTo('/');
+}
